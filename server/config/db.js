e45baadc9c9c8ddaa77dfa4840c5ab2bb8704bb6
Sandip KC <sandipkc841@gmail.com>
2023-03-02 01:02:50 -0500

    Second Commit

---
+++ server/config/db.js
@@ -0,0 +1,8 @@
+// mongodb set up ->
+// Author: Sandip K C ->
+// Student ID: 301206086 ->
+// Booklist Web App ->
+module.exports = {
+  // MongoDB deployment ->
+  "URI": "mongodb+srv://anyone:6IMUQpCeB4SzJonD@cluster0.u1pcv3s.mongodb.net/book_store?retryWrites=true&w=majority"
+};
---
+++ server/models/books.js
@@ -0,0 +1,19 @@
+// Author: Sandip K C ->
+// Student ID: 301206086 ->
+// Booklist Web App ->
+
+let mongoose = require('mongoose');
+
+// create a model class
+let Book = mongoose.Schema({
+    Title: String,
+    Description: String,
+    Price: Number,
+    Author: String,
+    Genre: String
+},
+{
+  collection: "books"
+});
+
+module.exports = mongoose.model('Book', Book);
